d0a394d2149361231b7f5c2e67d7f14f612dd2b8
shinsukeito <shinsukeito@live.com.au>
2022-11-27 19:17:55 +1100

    Tooltips now pivot based on screen position

--- Assets/Scripts/UI/TooltipSystem.cs
+++ Assets/Scripts/UI/TooltipSystem.cs
@@ -13,15 +13,42 @@ public class TooltipSystem : MonoBehaviour
 	public TMP_Text textCost;
 	public TMP_Text textTooltip;
 
+	public RectTransform rect;
+
 	// Start is called before the first frame update
 	void Start()
 	{
+		rect = tooltip.GetComponent<RectTransform>();
 		current = this;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+
+		Vector2 position = Input.mousePosition;
+		float px = 0;
+		float py = -0.1f;
+
+		if (position.x < Screen.width / 2)
+		{
+			px = 0;
+		}
+		else
+		{
+			px = 1f;
+		}
+
+		if (position.y < Screen.height / 2)
+		{
+			py = -0.1f;
+		}
+		else
+		{
+			py = 1.1f;
+		}
+
+		rect.pivot = new Vector2(px, py);
 	}
 
 	public static void ShowText(string title, string cost, string text)
