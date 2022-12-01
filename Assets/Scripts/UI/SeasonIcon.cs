58b2cafeec91291955582a137a44cececb930158
winged_souls <wingedsoulsmusic@gmail.com>
2022-12-01 15:08:38 +1100

    Changed season tooltips

--- Assets/Scripts/UI/SeasonIcon.cs
+++ Assets/Scripts/UI/SeasonIcon.cs
@@ -44,33 +44,33 @@ public class SeasonIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 				break;
 			case Season.Cold:
 				seasonImage.sprite = coldIcon;
-				tooltipTitle = "Cold Season";
-				tooltipText = "Spawns <color=#74F5FD>the Frosts</color> in a random area";
-				tooltipText += "\n\n<color=#74F5FD>The Frosts</color>";
+				tooltipTitle = "The Frosts";
+				tooltipText = "Spawns a <color=#74F5FD>blizzard</color> in a random area";
+				tooltipText += "\n\n<color=#74F5FD>Blizzard</color>";
 				tooltipText += "\n - Chance to highly damage forests";
 				tooltipText += "\n - Forests have a chance not to grow";
 				break;
 			case Season.Dry:
 				seasonImage.sprite = dryIcon;
-				tooltipTitle = "Dry Season";
-				tooltipText = "Spawns <color=#FBEAA3>the Famine</color> in a random area";
-				tooltipText += "\n\n<color=#FBEAA3>The Famine</color>";
+				tooltipTitle = "The Famine";
+				tooltipText = "Spawns a <color=#FBEAA3>drought</color> in a random area";
+				tooltipText += "\n\n<color=#FBEAA3>Drought</color>";
 				tooltipText += "\n - Hinders forests from growing";
 				tooltipText += "\n - Low but steady damage to forests";
 				break;
 			case Season.Hot:
 				seasonImage.sprite = hotIcon;
-				tooltipTitle = "Hot Season";
-				tooltipText = "Spawns <color=#E37840>the Burns</color> in a random location that spreads over time";
-				tooltipText += "\n\n<color=#E37840>the Burns</color>";
+				tooltipTitle = "The Burns";
+				tooltipText = "Spawns a <color=#E37840>fire</color> in a random location that spreads over time";
+				tooltipText += "\n\n<color=#E37840>Fire</color>";
 				tooltipText += "\n - Damages forests";
 				tooltipText += "\n - Has a chance to spread each day";
 				break;
 			case Season.Wet:
 				seasonImage.sprite = wetIcon;
-				tooltipTitle = "Wet Season";
-				tooltipText = "Spawns <color=#1475C0>the Rains</color> near rivers";
-				tooltipText += "\n\n<color=#1475C0>The Rains</color>";
+				tooltipTitle = "The Rains";
+				tooltipText = "<color=#1475C0>Floods</color> land near rivers";
+				tooltipText += "\n\n<color=#1475C0>Flood</color>";
 				tooltipText += "\n - Damages forests";
 				tooltipText += "\n - Deals more damage the longer it remains";
 				break;
